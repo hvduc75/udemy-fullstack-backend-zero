2e392f0f991cfb6270e8a619aa461f5a7724ecf5
Hoang Van Duc <duch52362@gmail.com>
2024-03-13 15:51:21 +0700

    Bai 31

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require("express"); // commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
@@ -15,21 +14,10 @@ configViewEngine(app);
 //khai báo route
 app.use('/', webRoutes);
 
-// Test connection
-// Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307, // default: 3306
-  user: 'root', // default: empty
-  password: '123456',
-  database: 'hoidanit',
-});
-
 connection.query(
   'SELECT * FROM Users u',
   function (err, results, fields) {
     console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
   }
 )
 
